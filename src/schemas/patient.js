5b773793d3aa899be3a910c01ecad5394dea1efb
David Sliren <bluee.97.berry@gmail.com>
2026-08-23 00:20:45 -0500

    refactor(schema/patient): agregar validacion para imagenes de manera opcional

--- src/schemas/patient.js
+++ src/schemas/patient.js
@@ -3,6 +3,7 @@ import z from "zod";
 export const patientSchema = z.object({
   name: z.string().min(3, "Debe tener minimo 3 caracteres"),
   lastName: z.string().min(3, "Debe tener minimo 3 caracteres"),
+  img: z.string().min(3, "Debe tener minimo 5 caracteres").optional(),
   age: z.number().min(1, "Debe tener como minimo 1 año"),
   userId: z.string().regex(/^[0-9a-fA-F]{24}$/, "el id no es valido"),
   goal: z.number().min(100000, "Debe ser mayor a 100000"),
