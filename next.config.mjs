c4ffd07fba5ec1cf963c476d4e31c887d007c8f2
David Sliren <bluee.97.berry@gmail.com>
2026-08-23 00:21:50 -0500

    feact(next/config): agregar soporte para imagenes remotas de cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   allowedDevOrigins: ["pregnancy-judiciary-specked.ngrok-free.dev"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
